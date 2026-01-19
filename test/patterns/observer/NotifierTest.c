d71d28a182d3a20e2e45d929c64ecac835ec7550
Saad <saadmuizz@gmail.com>
2026-01-19 17:58:51 -0500

    test5

--- test/patterns/observer/NotifierTest.c
+++ test/patterns/observer/NotifierTest.c
@@ -39,7 +39,7 @@ void testInstance() {
     assert(notifier != NULL);
     assert(notifier->getFacade(notifier) != NULL);
 
-    // puremvc_facade_removeFacade("Test1");
+    puremvc_facade_removeFacade("Test1");
     // puremvc_notifier_free(&notifier);
 }
 
