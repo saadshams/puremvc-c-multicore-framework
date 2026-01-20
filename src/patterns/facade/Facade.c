09adf44a0d6b3d169796b389f211bf664dbca509
Saad <saadmuizz@gmail.com>
2026-01-20 10:58:43 -0500

    update

--- src/patterns/facade/Facade.c
+++ src/patterns/facade/Facade.c
@@ -196,9 +196,9 @@ void puremvc_facade_removeFacade(const char *key) {
     assert(key != NULL);
     mutex_lock(&mutex);
 
-    puremvc_model_removeModel(key);
-    puremvc_view_removeView(key);
-    puremvc_controller_removeController(key);
+    // puremvc_model_removeModel(key);
+    // puremvc_view_removeView(key);
+    // puremvc_controller_removeController(key);
 
     struct IFacade *facade = instanceMap->removeItem(instanceMap, key);
     puremvc_facade_free(&facade);
